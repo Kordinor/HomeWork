452c6985de5d5701bc7a63a512dceb3b09ee78e0
Kordinor <kostya.shramenko@gmail.com>
2018-07-04 15:08:56 +0300

    4

---
+++ DZ01/Rating.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {   
+        Console.WriteLine("Введите ваш балл от 1 до 100");
+        int a = Int32.Parse(Console.ReadLine());
+        int counter = -1;
+        bool action = false;
+        string[] A ={"A", "B", "C", "D"};
+        for(int i=95; i>=50; i-=15){
+            i+=5*counter;
+	        if(a>=i){
+        		Console.WriteLine(A[counter+1]);
+        		action = true;
+        		break;
+	}
+	        counter++;
+}
+        if(!action){
+	        Console.WriteLine("E");
+}
+    }
+}
